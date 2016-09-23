2809e80c8fa6ed7ee06da30094602a2041d5bb22
Nathan Wilkinson <nathanwilk7@gmail.com>
2016-09-23 00:51:56 -0600

    bnlearn, coded simple linear regression, applied r linear regression

---
+++ r/bnlearn/examples-dag.R
@@ -0,0 +1,30 @@
+install.packages('bnlearn')
+e = empty.graph(LETTERS[1:6])
+e
+class(e)
+empty.graph(LETTERS[1:6], num = 2)
+arc.set = matrix(c("A", "C", "B", "F", "C", "F"),
+                 ncol = 2, byrow = TRUE,
+                 dimnames = list(NULL, c("from", "to")))
+arc.set
+arc(e) = arc.set
+arcs(e) = arc.set
+e
+bogus = matrix(c("X", "Y", "W", "Z"), 
+               byrow = TRUE, ncol=2,
+               dimnames = list(NULL, c("from", "to")))
+bogus
+arcs(e) = bogus
+adj = matrix(0L, nrow=6, ncol=6, dimnames=list(LETTERS[1:6],LETTERS[1:6]))
+adj
+adj["A", "C"] = 1L
+adj["B", "F"] = 1L
+adj["C", "F"] = 1L
+adj["D", "E"] = 1L
+adj["A", "E"] = 1L
+adj
+amat(e) = adj
+e
+model2network("[A][C][B|A][D|C][F|A:B:D][E|F]")
+modelstring(e) = "[A][C][B|A][D|C][F|A:B:C][E|F]"
+e
